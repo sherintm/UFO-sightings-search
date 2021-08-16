1f9b886b9554856fe884a712b938cec56aca4fc4
Sherin Thomas <sherinmt@gmail.com>
2021-08-16 16:59:03 +1000

    Added few more comments

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,71 +1,35 @@
 // from data.js
 let tableData = data;
 
-
-let filterBtn = d3.select('#filter-btn')
+// Add the event listener to filter data
+filterBtn = d3.select('#filter-btn')
 filterBtn.on('click', filterData)
 
-let datetime = d3.select('#datetime')
-let citySelect = d3.select('#city')
-let stateSelect = d3.select('#state')
-let countrySelect = d3.select('#country')
-let shapeSelect = d3.select('#shape')
+// Datetime selector
+datetime = d3.select('#datetime')
 
-
-let cities = [...new Set(tableData.map((row) => row.city))]
-let states = [...new Set(tableData.map((row) => row.state))]
-let countries = [...new Set(tableData.map((row) => row.country))]
-let shapes = [...new Set(tableData.map((row) => row.shape))]
-
-cities.forEach((city) => {
-    addOptions(citySelect, city)
-})
-
-states.forEach((state) => {
-    addOptions(stateSelect, state)
-})
-
-countries.forEach((country) => {
-    addOptions(countrySelect, country)
-})
-
-shapes.forEach((shape) => {
-    addOptions(shapeSelect, shape)
-})
-
-
-function addOptions(dropDown, option)
-{
-    var options = dropDown.append("option");
-    options.text(option)
-    options.attr("value",option)
-}
-
-
+// Event listener callback
 function filterData()
 {
     // Prevent the page from refreshing
     d3.event.preventDefault();
+    date = datetime.property("value")
 
-    let date = datetime.property("value")
-    let city = citySelect.property("value")
-    let state = stateSelect.property("value")
-    let country = countrySelect.property("value")
-    let shape = shapeSelect.property("value")
+    console.log(date);
 
-    filteredData = tableData.filter( row => row.datetime === date && 
-                                            row.city === city &&
-                                            row.state === state &&
-                                            row.country === country &&
-                                            row.shape === shape)
+    filteredData = tableData.filter( row => row.datetime === date)
+    console.log(filterData);
 
     let tbody = d3.select('tbody')
     tbody.html('&nbsp;')
     filteredData.forEach(row => {
         tbody.append('tr')
-        Object.entries(row).forEach(([key, value]) =>
-        {
-            tbody.append('td').text(value)
-        })
+        tbody.append('td').text(row.datetime)
+        tbody.append('td').text(row.city)
+        tbody.append('td').text(row.state)
+        tbody.append('td').text(row.country)
+        tbody.append('td').text(row.shape)
+        tbody.append('td').text(row.durationMinutes)
+        tbody.append('td').text(row.comments)
     });
 }
--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -1,33 +1,75 @@
 // from data.js
 let tableData = data;
 
-
-filterBtn = d3.select('#filter-btn')
+// Add the event listener to filter data
+let filterBtn = d3.select('#filter-btn')
 filterBtn.on('click', filterData)
 
-datetime = d3.select('#datetime')
+// Datetime selector
+let datetime = d3.select('#datetime')
 
+// Dropdown selectors
+let citySelect = d3.select('#city')
+let stateSelect = d3.select('#state')
+let countrySelect = d3.select('#country')
+let shapeSelect = d3.select('#shape')
+
+// Find unique entries for each field
+let cities = [...new Set(tableData.map((row) => row.city))]
+let states = [...new Set(tableData.map((row) => row.state))]
+let countries = [...new Set(tableData.map((row) => row.country))]
+let shapes = [...new Set(tableData.map((row) => row.shape))]
+
+// Add options for each dropdown
+cities.forEach((city) => {
+    addOptions(citySelect, city)
+})
+
+states.forEach((state) => {
+    addOptions(stateSelect, state)
+})
+
+countries.forEach((country) => {
+    addOptions(countrySelect, country)
+})
+
+shapes.forEach((shape) => {
+    addOptions(shapeSelect, shape)
+})
+
+// Function to add options to dropdown
+function addOptions(dropDown, option)
+{
+    var options = dropDown.append("option");
+    options.text(option)
+    options.attr("value",option)
+}
+
+// Event listener callback
 function filterData()
 {
     // Prevent the page from refreshing
     d3.event.preventDefault();
-    date = datetime.property("value")
 
-    console.log(date);
+    let date = datetime.property("value")
+    let city = citySelect.property("value")
+    let state = stateSelect.property("value")
+    let country = countrySelect.property("value")
+    let shape = shapeSelect.property("value")
 
-    filteredData = tableData.filter( row => row.datetime === date)
-    console.log(filterData);
+    filteredData = tableData.filter( row => row.datetime === date && 
+                                            row.city === city &&
+                                            row.state === state &&
+                                            row.country === country &&
+                                            row.shape === shape)
 
     let tbody = d3.select('tbody')
     tbody.html('&nbsp;')
     filteredData.forEach(row => {
         tbody.append('tr')
-        tbody.append('td').text(row.datetime)
-        tbody.append('td').text(row.city)
-        tbody.append('td').text(row.state)
-        tbody.append('td').text(row.country)
-        tbody.append('td').text(row.shape)
-        tbody.append('td').text(row.durationMinutes)
-        tbody.append('td').text(row.comments)
+        Object.entries(row).forEach(([key, value]) =>
+        {
+            tbody.append('td').text(value)
+        })
     });
 }
